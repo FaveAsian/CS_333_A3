a18ccd907cd1128d84ed2cd76e5dce3fa7b064fa
FaveAsian <38636488+FaveAsian@users.noreply.github.com>
2023-11-24 13:09:17 -0600

    added country name hover

--- visual.js
+++ visual.js
@@ -10,6 +10,10 @@ let svg = d3.select("#map-container").append("svg")
     .attr("width", width)
     .attr("height", height);
 
+let tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip");
+
 // https://www.d3indepth.com/zoom-and-pan/
 let zoom = d3.zoom()
     .scaleExtent([1, 8])
@@ -31,7 +35,9 @@ async function ready(){
         .enter().append("path")
         .attr("d", path)
         .attr("class", "country")
-    
+        .on("mouseover", mouseOverEvent)
+        .on("mouseout", mouseOutEvent);
+
     // Add a border around the map
     svg.append("rect")
         .attr("x", 0)
@@ -48,4 +54,19 @@ function handleZoom(e){
     //  Easy zooming and panning
     svg.selectAll("path")
         .attr("transform", e.transform)
+}
+
+function mouseOverEvent(d){
+    let countryData = d3.select(this).datum();
+    let [x, y] = d3.pointer(d);
+
+    tooltip.html(countryData.properties.name)
+        .style("left", (x + 10) + "px")
+        .style("top", (y - 15) + "px")
+        .style("display", "block");
+}
+
+function mouseOutEvent(d){
+    // Hide tooltip on mouseout
+    tooltip.style("display", "none");
 }
